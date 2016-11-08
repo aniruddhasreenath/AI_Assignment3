8f480623c43de56eb456a8bdcf8a8cb7a01cc506
Sreenath <as17@illinois.edu>
2016-11-08 02:25:17 -0600

    testing almost done

--- Test.java
+++ Test.java
@@ -17,8 +17,64 @@ public class Test {
 
     public void test(){
 
-        
+        for(int i = 0; i < testData.size(); i ++){
 
+            //get the first image and classify it
+            Image newImage = testData.get(i);
+
+            for(int row = 0; row < 28; row++){
+                for(int col = 0; col < 28; col++){
+                    char fVal = newImage.image[row][col];
+
+
+                }
+            }
+
+        }
+
+    }
+
+    public int classify(char f, int i, int j){
+
+        int numberClass = 0;
+
+        //this is the key for the hashmap
+        String key = Integer.toString(i)+Integer.toString(j);
+
+        //an index that helps in retriving the correct prob from the list
+        int fSymbKey = 0;
+        if (f == ' ') fSymbKey = 2;
+        if (f == '#') fSymbKey = 1;
+        if (f == '+') fSymbKey = 0;
+
+        //get the probability matrix for this key
+        ArrayList<double[]> probabilities = knowledge.get(key);
+
+        //store the relavent probs here
+        ArrayList<Double> probs = new ArrayList<Double>();
+
+        //interate over all the classes in teh prob list and find the maximum value for that symbol
+        double maxProb = 0;
+
+        //extract the relavent probabilities
+        for(int clas = 0; clas < probabilities.size(); clas ++){
+            probs.add(probabilities.get(clas)[fSymbKey]);
+        }
+
+        maxProb = chooseMax(probs);
+        
+    }
+
+    public double chooseMax(ArrayList<Double> list){
+
+        double max = 0;
+
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i) > max){
+                max = list.get(i);
+            }
+        }
+        return max;
     }
 
 
